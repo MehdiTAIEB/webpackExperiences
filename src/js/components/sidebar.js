3a5a335052454b7273242cdda21c48c8b8177bda
mehdi <mehdi.taieb@epitech.eu>
2017-08-01 16:00:38 +0200

    fget

---
+++ src/js/components/sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Sidebar extends React.Component {
+	render() {
+		return(
+			<div id="sidebar">
+				<h1>Sidebar Maine</h1>
+			</div>
+		);
+	}
+}
