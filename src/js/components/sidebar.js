c67909ab7bd54bdac7100e6c9ac6a9d50a818746
mehdi <mehdi.taieb@epitech.eu>
2017-08-01 17:48:38 +0200

    played with component interaction and value transfer

--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import List from './list';
+
 export default class Sidebar extends React.Component {
+	constructor() {
+		super();
+		// array of menu
+		// static or fetched from api
+		this.state = {
+			el: Array(10).fill({name:'asd', id: 2})
+		};
+	}
+	generateRows () {
+		var rows = [];
+		rows = this.state.el.map((e, i) =>
+			<List key={i} value={e.name} onClick={() => this.handleClick(e.id)}/>
+		);
+		return rows;
+	}
+	handleClick(i) {
+		alert(i);
+	}
+	renderList() {
+		var rows = this.generateRows();
+		return (
+			<div>
+			{rows}			
+			</div>
+		);
+	}
 	render() {
 		return(
 			<div id="sidebar">
 				<h1>Sidebar Maine</h1>
+				<ul>
+					{this.renderList()}
+				</ul>
 			</div>
 		);
 	}
